308f75458baf909590862c013ba4a6af21bdeb10
Francisco velasco conde <fvelasco@bankvision.com.co>
2020-01-13 21:22:42 -0500

    initial commit

---
+++ src/main/java/com/indra/finance/IndraFinanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.indra.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndraFinanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndraFinanceServiceApplication.class, args);
+	}
+
+}
